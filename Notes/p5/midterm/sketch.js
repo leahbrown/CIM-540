711679646d82e1209ae4906736e3fed49988fd46
Leah Brown <lrb107@miami.edu>
2019-03-05 15:28:22 -0500

    h

--- Notes/p5/midterm/sketch.js
+++ Notes/p5/midterm/sketch.js
@@ -18,14 +18,19 @@ var finishLine;
 
 var startButton;
 var resetButton;
-
-
+var greet;
+var submitButton;
 var startTimer = false;
 var prevTimer = 0;
 var interval = 3000;
 
-var avaWins = "Ava Wins!"
-var jimmyWins = "Jimmy Wins!"
+var avaWins = "Name 1 Wins!";
+var jimmyWins = "Name 2 Wins!";
+var input;
+var input2;
+
+var name1 = "Enter name";
+var name2 = "Enter name";
 function preload(){
   dog1 = loadImage("assets/dog1.png");
   dog2 = loadImage("assets/dog2.png");
@@ -37,16 +42,25 @@ function preload(){
 function setup() {
   // put setup code here
   createCanvas(700,700);
-
+  input = createInput();
+    input.position(20, 60);
+    input.input(function(){
+      name1 = input.value();
+    });
+    input2 = createInput();
+      input2.position(20, 435);
+      input2.input(function(){
+        name2 = input2.value();
+      });
   startButton = createButton("Start");
-  startButton.position(10,40);
+  startButton.position(10,20);
   startButton.mousePressed(function(){
     speed1 = random(0,5);
     speed2 = random(0,5);
     startRace = true;
   });
   resetButton = createButton("Reset");
-  resetButton.position(100,40);
+  resetButton.position(100,20);
   resetButton.mousePressed(function(){
     dog1X = 0;
     dog2X =0;
@@ -59,51 +73,46 @@ function setup() {
 function draw() {
   // put drawing code here
   background(255);
-
+//name1 = input.value();
 
   if(startRace == true){
     dog1X = dog1X + random(0,5);
     dog2X = dog2X +random(0,5);
+    prevTimer = millis();
   }
 
   if(dog1X > finishLineLocation){
       startRace = false;
 
-      if(startTimer == true){
 
-      text(jimmyWins, 375,225);
-    if(millis() - prevTimer >= interval){
-    startTimer = false;
-    prevTimer = millis();
-    }
-    }
+      text(name1, 375,225);
+      if(millis() - prevTimer >= interval){
+        dog1X = 0;
+        dog2X = 0;
+        prevTimer = millis();
+      }
+
   }
   if(dog2X > finishLineLocation){
     startRace = false;
 
-      if(startTimer == true){
-
-      text(avaWins, width/2,height/2);
+      text(name2, width/2,height/2);
       if(millis() - prevTimer >= interval){
-      startTimer = false;
-      prevTimer = millis();
+        dog1X = 0;
+        dog2X = 0;
+        prevTimer = millis();
       }
-      }
+
 
   }
 
-  //timer formula
-  startTimer = true;
-  prevTimer = millis();
-  //reset dog position
-
-
-
-
   image(finishLine,450,0);
-  image(dog1,dog1X,60,dog1.width/2,dog1.height/2);
-  text ("Jimmy",30,225);
-  image(dog2,dog2X,220,dog2.width/2,dog2.height/2);
-  text ("Ava",30,385);
+
+  image(dog1,dog1X,80,dog1.width/2,dog1.height/2);
+  text (name1,30,250);
+
+  image(dog2,dog2X,250,dog2.width/2,dog2.height/2);
+  text (name2,30,415);
+  text("Click start and watch the puppies race!",20,15);
 
 }
